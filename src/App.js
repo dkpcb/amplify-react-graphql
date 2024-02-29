056c2a3ef0a36ce912dceab3d81dc07edb74ad2f
加藤大地 <katoudaichi@katoudaichinoMacBook-Air.local>
2024-03-01 05:33:38 +0900

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -55,6 +55,50 @@ const App = ({ signOut }) => {
     });
   }
 
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
+    setNotes(notesFromAPI);
+  }
+
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const image = form.get("image");
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+      image: image.name,
+    };
+    if (!!data.image) await Storage.put(data.name, image);
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: data },
+    });
+    fetchNotes();
+    event.target.reset();
+  }
+  
+  async function deleteNote({ id, name }) {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+    await Storage.remove(name);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  }
+
+
   return (
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
@@ -100,6 +144,12 @@ const App = ({ signOut }) => {
           </Flex>
         ))}
       </View>
+      <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+            />
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
